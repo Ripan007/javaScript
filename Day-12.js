ed91b08da3f12838cf0b21ce09826b93da75604f
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2022-10-11 19:18:40 +0530

    added

---
+++ Day-12.js
@@ -0,0 +1,60 @@
+// button with id btn
+const myBtn = document.querySelector("#myBtn");
+//  div with id container
+
+const content = document.querySelector("#content");
+
+// fetch data
+
+// function getData() {
+//   url = "Day-12.txt";
+//   fetch(url)
+//     .then((response) => {
+//       return response.text();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     });
+// }
+
+// getData();
+
+// json data
+// parse data
+// object data
+// use
+
+// link api : https://github.com/public-apis/public-apis
+
+// function getData() {
+//   url = "https://random.dog/woof.json";
+//   fetch(url)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     });
+// }
+
+// getData();
+
+// we need to stringfy object
+// if data is object use stringfy
+// if it is  a string than dont use stringfy
+
+// link : https://dummy.restapiexample.com/
+
+// function postData() {
+//   url = "https://dummy.restapiexample.com/api/v1/create";
+//   data = '{"name":"rajjhhh","salary":"123","age":"23"}';
+//   parameter = {
+//     method: "post",
+//     headers: { "content-Type": "application/json" },
+//     body: data,
+//   };
+
+//   fetch(url, parameter)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+// }
