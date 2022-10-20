efa1fa5df8f2b7026f1181b9f1dffcdebfa5d2ef
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2022-10-20 17:22:19 +0530

    add

---
+++ Day-20.js
@@ -0,0 +1,45 @@
+// forEach
+
+// const arr = [1, 2, 3, 4];
+// for (i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function (value) {
+//     console.log(value);
+// })
+
+// arr.forEach(function (value, index) {
+//     console.log(index, value);
+// })
+
+// arr.forEach(function (value, index, arr) {
+//     console.log(arr[index] + 100);
+// })
+
+// for of loop
+
+// const arr = [1, 2, 3, 4];
+
+// for (item of arr) {
+//     console.log(item);
+// }
+
+
+// const text = ["a", "b", "c"];
+// for (item of text) {
+//     console.log(item);
+// }
+
+// for in
+
+// const user = {
+//     name: "raj",
+//     age: 20
+// }
+
+// for (prop in user) {
+//     console.log(prop);
+// }
+
+
