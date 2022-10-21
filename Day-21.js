bda327afb76408dc884f3e72329038f84eb09d48
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2022-10-21 23:27:09 +0530

    add

---
+++ Day-21.js
@@ -0,0 +1,77 @@
+// array
+// const friends = ['ajay', 'sami', 'komal'];
+// console.log(friends);
+// console.log(friends[0]);
+
+// const years = new Array(2000, 2001, 2002);
+// console.log(years);
+// console.log(years[0]);
+
+// calculate the length
+// console.log(friends.length);
+// last element
+// console.log(friends[friends.length - 1]);
+
+// change the element
+// friends[0] = 'kabita';
+// console.log(friends);
+
+// shows error
+// friends = ['rashmi', 'kushi'];
+// console.log(friends);
+
+// const calAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const years = [2000, 2001, 2004, 2005];
+
+// const age1 = calAge(years[0]);
+// const age2 = calAge(years[1]);
+// const age3 = calAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calAge(years[0]), calAge(years[1]), calAge(years[years.length - 1])];
+// console.log(ages);
+
+
+// add elements
+
+// const arr = ['raj', 'gautam', 'komal'];
+// add elements at last
+// arr.push('kabita');
+// add element at first
+// arr.unshift('rohan');
+// console.log(arr);
+
+
+// remove elements
+// arr.pop();
+// remove last elements
+
+// arr.shift();
+// remove first elements
+// console.log(arr);
+
+
+// check indexes
+
+// console.log(arr.indexOf('raj'));
+
+// check present or not
+// present than true else false
+
+// console.log(arr.includes('raj'));
+
+// if (arr.includes('raj')) {
+//     console.log('raj is present');
+// } else {
+//     console.log('not present');
+// }
+
+
+
+
+
+
+// object
