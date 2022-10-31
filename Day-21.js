0164f8c71e347a41aace2c17fe67b19c24bb7790
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2022-10-31 11:27:38 +0530

    add

--- Day-21.js
+++ Day-21.js
@@ -1,77 +1,134 @@
-// array
-// const friends = ['ajay', 'sami', 'komal'];
-// console.log(friends);
-// console.log(friends[0]);
+// console.log(2 + "2");
+// console.log(2 - "2");
+// bug
 
-// const years = new Array(2000, 2001, 2002);
-// console.log(years);
-// console.log(years[0]);
-
-// calculate the length
-// console.log(friends.length);
-// last element
-// console.log(friends[friends.length - 1]);
-
-// change the element
-// friends[0] = 'kabita';
-// console.log(friends);
-
-// shows error
-// friends = ['rashmi', 'kushi'];
-// console.log(friends);
-
-// const calAge = function (birthYear) {
-//     return 2037 - birthYear;
-// }
-
-// const years = [2000, 2001, 2004, 2005];
-
-// const age1 = calAge(years[0]);
-// const age2 = calAge(years[1]);
-// const age3 = calAge(years[years.length - 1]);
-// console.log(age1, age2, age3);
-
-// const ages = [calAge(years[0]), calAge(years[1]), calAge(years[years.length - 1])];
-// console.log(ages);
-
-
-// add elements
-
-// const arr = ['raj', 'gautam', 'komal'];
-// add elements at last
-// arr.push('kabita');
-// add element at first
-// arr.unshift('rohan');
-// console.log(arr);
-
-
-// remove elements
-// arr.pop();
-// remove last elements
-
-// arr.shift();
-// remove first elements
-// console.log(arr);
-
-
-// check indexes
-
-// console.log(arr.indexOf('raj'));
-
-// check present or not
-// present than true else false
-
-// console.log(arr.includes('raj'));
-
-// if (arr.includes('raj')) {
-//     console.log('raj is present');
-// } else {
-//     console.log('not present');
-// }
-
-
-
-
-
-
+// difference b/w null and undefined ?
+// null
+// const a = null;
+// console.log(a);
+// console.log(typeof (a));
 // object
+// bug
+
+// undefined
+// var b;
+// console.log(b);
+// console.log(typeof (b));
+
+// what is NaN ?
+// console.log("a" - "b");
+// var myNumber = 44444444;
+// var myName = "raj";
+
+// console.log(isNaN(myNumber));
+// console.log(isNaN(myName));
+
+// if (isNaN(myName)) {
+//     console.log("input valid elements");
+
+// }
+// increment and decrement operator
+// var num = 2;
+// var newNum = num++;
+// console.log(num);
+// console.log(newNum);
+
+// more practice required
+// ask in interview
+// postfix
+// var num = 2;
+// var newNum = num++ + 3;
+// console.log(num);
+// console.log(newNum);
+
+// prefix
+// var num = 2;
+// var newNum = ++num;
+// console.log(num);
+// console.log(newNum);
+
+// var num = 3;
+// var newNum = ++num + 5;
+// console.log(num);
+// console.log(newNum);
+
+// write a program to swap numbers
+// var a = 5;
+// var b = 10;
+// var temp;
+
+// temp = a;
+// a = b;
+// b = temp;
+// console.log(a);
+// console.log(b);
+
+//  swap number without using third variable
+
+// var a = 5;
+// var b = 10;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a);
+// console.log(b);
+
+// diff between == and === ?
+// write a program if the year is a leap year or not ?
+
+// var year = 2020;
+// if (year % 4 === 0) {
+//     if (year % 100 === 0) {
+//         if (year % 400) {
+//             console.log("leap year");
+//         } else {
+//             console.log("not a leap year");
+//         }
+//     } else {
+//         console.log("leap year");
+//     }
+// } else {
+//     console.log("not a leap year");
+// }
+
+// what is truthy an falsy value  ?
+
+// ternary operator
+// var age = 18;
+// console.log((age >= 18) ? "you can vote" : "you cant vote");
+
+// var num = 0;
+// while (num <= 20) {
+//     console.log(num);
+//     num++
+// }
+
+// print  a  table using forloop
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(2 * i);
+// }
+
+// function sum() {
+//     var a = 2;
+//     var b = 2;
+//     var total = a + b;
+//     console.log(total);
+// }
+
+// sum()
+
+// function sum(a, b) {
+//     var total = a + b;
+//     console.log(total);
+// }
+// sum(2, 2);
+
+
+
+
+
+
+
+
+
